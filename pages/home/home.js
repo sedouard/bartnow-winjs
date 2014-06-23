674a473b2fb7a5f24a2dae4b8fd9d5b33e059218
sedouard <steven.edouard1@gmail.com>
2014-06-23 01:03:20 -0700

    adding icons for stations

--- pages/home/home.js
+++ pages/home/home.js
@@ -56,13 +56,13 @@
                         }
 
                         var infoOptions = {
-                            title:model.attributes.name, 
+                            title:model.attributes.name,
                             width: 200, 
                             height:100,
                             description: descriptionString
                         };
                         var pinInfobox = new Microsoft.Maps.Infobox(stationLoc, infoOptions);
-                        var stationPin = new Microsoft.Maps.Pushpin(stationLoc, {infobox: pinInfobox, icon: '/images/bart-300x300.png'});
+                        var stationPin = new Microsoft.Maps.Pushpin(stationLoc, {infobox: pinInfobox, icon: '/images/bartIcon20x20.png',  width: 20, height: 20});
                         map.entities.push(pinInfobox);
                         map.entities.push(stationPin);
                     }
