a98489fdbce6ce7621ba91b3351859765577ab26
sedouard <steven.edouard1@gmail.com>
2014-06-24 01:22:52 -0700

    fix undefines in station view

--- pages/station/station.js
+++ pages/station/station.js
@@ -12,6 +12,26 @@
         ready:function(element, station){
             var pivot = $("#pivotScenario3")[0].winControl;
             pivot.title = station.attributes.name;
+
+            for(var z in station.attributes.etd){
+                if(!station.attributes.etd[z].estimate){
+                    station.attributes.etd.estimate = [];
+                }
+
+                if(station.attributes.etd[z].estimate.length < 3){
+                    var len = station.attributes.etd[z].estimate.length;
+                    for(var i = 0; i < 3 - len; i++){
+
+                        station.attributes.etd[z].estimate.push({
+                            minutes: "",
+                            color: "BLACK" 
+                        });
+                        
+                    }
+                }
+            }
+            
+
             var stationData = new WinJS.Binding.List(station.attributes.etd);
             window.StationName = station.attributes.name;
             WinJS.Namespace.define('BartNow.Station', {
