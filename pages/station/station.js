48b4a79b0cdf94ff73d75531ade8e5142bca62b1
sedouard <steven.edouard1@gmail.com>
2014-06-23 22:56:50 -0700

    temp fix for the staions page

--- pages/station/station.js
+++ pages/station/station.js
@@ -10,8 +10,10 @@
     
     WinJS.UI.Pages.define("/pages/station/station.html", {
         ready:function(element, station){
-           
-            var stationData = new WinJS.Binding.List([]);
+            
+            var stationArray = [];
+
+            var stationData = new WinJS.Binding.List(stationArray);
             window.StationName = "";
             var data = {
                 stationData : stationData,
@@ -24,14 +26,20 @@
                 var pivot = $("#pivotScenario3")[0].winControl;
                 pivot.title = model.attributes.name;
 
-
-                //TODO: Fix binding issue here
                 for(var i in station.attributes.etd){
-                    stationData.push(model.attributes.etd[i]);
+
+                    //TODO WinJS wants a binding function. I'm sure I'm doing this wrong, but things seem to work here
+                    station.attributes.etd[i].bind = function(property){
+                        return this[property];
+                        
+                    };
+
+                    data.stationData.push(station.attributes.etd[i]);
                 }
 
                 window.StationName = model.attributes.name;
                 data.name = model.attributes.name;
+                //data.stationData = new WinJS.Binding.List([]);
             });
             
         }
